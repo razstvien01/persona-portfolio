0de772d290f234b6c3e27d3f77bc9c5848b5a5ce
razstvien01 <nicolenevanz.aricayos@gmail.com>
2023-12-28 16:15:49 +0800

    added logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
+import { BrowserRouter } from "react-router-dom";
+import {
+  About,
+  Experience,
+  Feedbacks,
+  Tech,
+  Navbar,
+  Contact,
+  StarsCanvas,
+  Works,
+} from "./components";
 
 const App = () => {
-
   return (
-    <>
-      My Porfolio
-    </>
-  )
-}
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern">
+          <Navbar/>
+          <About/>
+          <Experience/>
+          <Tech/>
+          <Works/>
+          <Feedbacks/>
+          <div className="relative z-0">
+            <Contact/>
+            <StarsCanvas/>
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
