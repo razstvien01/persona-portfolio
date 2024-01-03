a31f5e1a994457c832ac6adb3c21f4675f099cfd
razstvien01 <nicolenevanz.aricayos@gmail.com>
2024-01-03 16:19:55 +0800

    tailwindcss applied

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,35 +2,39 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import { styles } from "../styles";
-import { navLinks } from '../constants'
+import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
-  
+
   return (
-    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
           to={"/"}
           className="flex items-center gap-2"
           onClick={() => {
             setActive("");
-            window.scrollTo(0, 0)
+            window.scrollTo(0, 0);
           }}
         >
           <img
             src={logo}
             alt="logo"
-            width={150}
-            height={100}
-            className="object-contain"
+            // width={150}
+            // height={100}
+            className="w-20 h-20 object-contain"
           />
-          
+          <p className="text-white text-[18px] font-bold cursor-pointer">
+            Nicolen <span className="sm:block hidden">Evanz Aricayos</span>
+          </p>
         </Link>
       </div>
     </nav>
-  )
+  );
 };
 
 export default Navbar;
