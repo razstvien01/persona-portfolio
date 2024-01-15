a07c36cb5e4a01983fdc612cc013547b0221d89b
razstvien01 <nicolenevanz.aricayos@gmail.com>
2024-01-15 21:19:28 +0800

    will adjust the computer size when in mobile size

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -15,7 +15,7 @@ import {
 
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
+const Computers = ({ isMobile}) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
@@ -32,8 +32,8 @@ const Computers = () => {
       />
       <primitive 
         object={computer.scene} 
-        scale={0.75}
-        position={[0, -3.25, -1.5]}
+        scale={isMobile ? 0.7 : 0.75 }
+        position={isMobile ? [0, -3, -2.2] :[0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -41,6 +41,29 @@ const Computers = () => {
 };
 
 const ComputerCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false)
+  
+  useEffect(() => {
+    //* add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia('(max-width: 500px)')
+    
+    //* set the initial value of the 'isMobile' state variable
+    setIsMobile(mediaQuery.matches)
+    
+    //* Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches)
+    }
+    
+    //* add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener('change', handleMediaQueryChange)
+  
+    //* remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange)
+    }
+  }, [])
+  
   return (
     <Canvas
       frameloop="demand"
@@ -61,7 +84,7 @@ const ComputerCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2} //* ONLY rotate right and left specific angle
         /> 
-        <Computers/>
+        <Computers isMobile={isMobile}/>
       </Suspense>
       
       <Preload all/>
