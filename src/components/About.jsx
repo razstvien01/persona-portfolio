10cba5d266991302300d554a53918d479af2f036
razstvien01 <nicolenevanz.aricayos@gmail.com>
2024-01-18 17:50:14 +0800

    added cards for the services provided

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,16 +1,37 @@
-import React from "react";
 //* mag tilt sa card
 import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
-import { services } from '../constants'
+import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
-const ServiceCard = ({ index, title, icon }) => {
-  return (
-    <p>{title}</p>
-  )
-}
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
 
 const About = () => {
   return (
@@ -22,16 +43,17 @@ const About = () => {
 
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
-        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a skilled software developer with experience in
-        Typescript/Javascript and expertise in frameworks like ReactJS, Node.js,
-        and NextJS
+        I'm a skilled software developer with experience in TypeScript,
+        JavaScript, Dart, Python and expertise in frameworks like React, Node.js, Flask, and Flutter. I'm a quick learner and collaborate closely with clients to
+        create efficient, scalable, and user-friendly solutions that solve
+        real-world problems. Let's work together to bring your ideas to life!
       </motion.p>
-      
-      <div className="mt-20 flex flex-wrap gap-10">
+
+      <div className='mt-20 flex flex-wrap gap-10'>
         {services.map((service, index) => (
-          <ServiceCard key={service.title} index={index} {...service}/>
+          <ServiceCard key={service.title} index={index} {...service} />
         ))}
       </div>
     </>
